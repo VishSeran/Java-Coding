af290adf0f457d6aba2b064b2f5a753c09839371
VishSeran <vishwa.sovis@gmail.com>
2026-02-05 17:05:06 +0530

    Excerption handling

---
+++ src/UserInputValidation.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Scanner;
+
+public class UserInputValidation {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        try {
+            float number = Float.parseFloat(scanner.nextLine());
+            System.out.println("The number you entered: " + number);
+        } catch (NumberFormatException e) {
+            System.err.println("Please enter a valid number!");
+        }finally{
+            System.out.println("Process over");
+        }
+    }
+}
