cd580d833f49dd85377c587b1ef51c7faea9838c
VishSeran <vishwa.sovis@gmail.com>
2026-02-07 18:38:15 +0530

    OOP advance concept

---
+++ src/Book.java
@@ -0,0 +1,30 @@
+public class Book {
+    
+    private String title;
+    private String author;
+    private float price;
+    
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public void setPrice(float price){
+        this.price = price;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+
+    public float getPrice(){
+        return this.price;
+    }
+}
