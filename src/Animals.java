c695c54c6738f2b21893c37d987657c5e40ebde9
VishSeran <vishwa.sovis@gmail.com>
2026-02-09 17:44:03 +0530

    Inheritance updated

---
+++ src/Animals.java
@@ -0,0 +1,40 @@
+//inheritance
+/*
+Inheritance use to use and iherit properties and methods to subclasses from one or more super classes.
+A subclass inherits all the public and protected members, such as fields and methods, from the superclass. However, it does not inherit the private members.
+
+There are 4 types of inheritances:
+
+1. Single inheritance: allows class to inherit from one super class
+2. Multi-level inheritance: allow class to inherit from another class which is also a subclass to a another super class.
+3. Hierarchical inheritance: allow muliple classes to iherit from single super class.
+4. Multple inhritance: allow class to inherit attributes and methods from multiple super classes.
+
+create each file for each class defined. Its the industrial best practice.
+
+for inherit from a super class we use extend keyword.
+ */
+public class Animals {
+    
+    protected String color;
+    protected String name;
+
+     void sound(){
+        System.out.println("animals have differnet sounds");
+    }
+}
+
+class Dog extends Animals {
+    protected String status;
+
+    public void eat(){
+        System.out.println(name +"is eating");
+    }
+
+    @Override
+    void sound(){
+        System.out.println("Dogs are barking");
+    }
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+      public static void main(String[] args) {
+        Dog mydog = new Dog();
+        mydog.name = "Buddy";
+        mydog.color  = "Black";
+        mydog.status = "puppy";
+
+        mydog.sound();
+    }
+}
