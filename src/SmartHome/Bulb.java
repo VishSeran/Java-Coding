7995e6ca72415b39b08ab31088255ba4bda94cec
VishSeran <vishwa.sovis@gmail.com>
2026-02-10 14:22:45 +0530

    smart home bulb.java updated

---
+++ src/SmartHome/Bulb.java
@@ -0,0 +1,23 @@
+
+interface Switchable {
+
+    void turnOn();
+
+    void turnOff();
+}
+
+interface Adjustable {
+
+    void increase();
+
+    void decrease();
+}
+
+interface Connectable {
+
+    void Connected();
+
+    void Disconnected();
+}
+
+
