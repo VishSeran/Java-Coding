41af77b9e8dc5c8b8c7a6ca974ca9ffcbf2c2042
VishSeran <vishwa.sovis@gmail.com>
2026-02-10 14:42:06 +0530

    smart bulb class created

--- src/SmartHome/Bulb.java
+++ src/SmartHome/Bulb.java
@@ -1,4 +1,6 @@
 
+
+
 interface Switchable {
 
     void turnOn();
@@ -19,5 +21,59 @@ interface Connectable {
 
     void Disconnected();
 }
+
+class SmartBulb implements Switchable, Adjustable, Connectable {
+
+    private Boolean isOn = false;
+    private int brightness = 50;
+    private Boolean isConnected = false;
+
+    @Override
+    public void turnOn(){
+       isOn = true;
+       System.out.println("Smart bulb is turned ON");
+    } 
+
+    @Override
+    public void turnOff(){
+        isOn = false;
+        System.out.println("Smart bulb is turned OFF");
+    }
+
+    @Override
+    public void increase(){
+        if(brightness <= 100){
+            brightness += 10;
+            System.out.println("Brightness: " + brightness + "%");
+        }else{
+            System.out.println("Brightness already at the Maximum");
+        }
+    }
+
+    @Override
+    public void decrease(){
+        if(brightness > 0) {
+            brightness -= 10;
+            System.out.println("Brightness: " + brightness + "%");
+        }else{
+            System.out.println("Brightness already at minimum");
+        }
+    }
+
+    @Override
+    public void Connected(){
+        isConnected = true;
+        System.out.println("Smart bulb is connected");
+    }
+
+    @Override
+    public void Disconnected(){
+        isConnected = false;
+        System.out.println("Smart bulb is disconnected");
+    }
+
+}
+
+class 
 
 
