3bf223d165c8145cd89314ef26384690f4393995
VishSeran <vishwa.sovis@gmail.com>
2026-02-11 18:53:38 +0530

    HashMap implemented

---
+++ src/Collection/Maps/HashMapExample.java
@@ -0,0 +1,24 @@
+package Collection.Maps;
+
+import java.util.HashMap;
+
+public class HashMapExample {
+    
+    public static void main(String[]args){
+
+
+        //hashmap and treemap allow null values.
+        //hashmap does not gurantee the data exists
+        //treemap store the key as sorted order
+        //keys must be unique
+        //values can duplicate
+
+        HashMap<String, Integer> age = new HashMap<>();
+
+        age.put("Nimal", 30);
+        age.put("Sunitha", 30);
+        age.put("Nimal", 40);
+
+        System.out.println("age list: " + age);
+    }
+}
