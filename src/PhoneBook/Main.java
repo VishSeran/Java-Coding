12cc97d703f4fc630dec87aa51040ef12b82d3c8
VishSeran <vishwa.sovis@gmail.com>
2026-02-12 19:14:32 +0530

    entry addition PhoneBook updated

--- src/PhoneBook/Main.java
+++ src/PhoneBook/Main.java
@@ -4,20 +4,53 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    
-    public static void main (String [] args) {
 
-        HashMap <String,String> phoneBook = new HashMap<>();
+    public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        try {
+            HashMap<String, String> phoneBook = new HashMap<>();
 
-        while(true){
-            System.out.println("\n Enter 1 to add an entry: " +
-            "\n Enter 2 to view phone book: " + 
-            "\n Enter 3 to search entry by name: " +
-            "\n Enter 4 to modify entry: " +
-            "\n Enter 5 to delete an entry: " +
-            "\n Enter Any other number to exit...");
+            Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+                System.out.println("\n Enter 1 to add an entry: "
+                        + "\n Enter 2 to view phone book: "
+                        + "\n Enter 3 to search entry by name: "
+                        + "\n Enter 4 to modify entry: "
+                        + "\n Enter 5 to delete an entry: "
+                        + "\n Enter Any other number to exit...");
+
+                String userSelection = scanner.nextLine();
+
+                if (userSelection.equals("1")) {
+
+                    System.out.println("Create an Entry");
+                    System.out.println("===============\n");
+
+                    System.out.println("Enter a name: ");
+                    String name = scanner.nextLine();
+
+                    if (!PhoneBookHashMap.isNameValid(name)) {
+                        System.out.println("Enter a valid name!");
+
+                    } else if (phoneBook.containsKey(name)) {
+                        System.out.println("\nThe name is already exists!");
+                        
+                    } else {
+                        System.err.println("\n Enter phone number");
+                        String number = scanner.nextLine();
+
+                        if (!PhoneBookHashMap.isNumberValid(number)) {
+                            System.err.println("\n Enter a valid phone number!");
+                        } else {
+                            System.out.println("\nEntry adding...");
+                            phoneBook.put(name, number);
+                        }
+                    }
+                }
+
+            }
+        } catch (Exception e) {
         }
 
     }
