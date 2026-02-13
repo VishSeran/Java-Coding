6da7d9c4639579095cf932fbc2cf0dcce5d11a64
VishSeran <vishwa.sovis@gmail.com>
2026-02-13 11:27:06 +0530

    delete by name updated

--- src/PhoneBook/Main.java
+++ src/PhoneBook/Main.java
@@ -15,9 +15,8 @@ public class Main {
             while (true) {
                 System.out.println("\n Enter 1 to add an entry: "
                         + "\n Enter 2 to view phone book: "
-                        + "\n Enter 3 to search entry by name: "
-                        + "\n Enter 4 to modify entry: "
-                        + "\n Enter 5 to delete an entry: "
+                        + "\n Enter 3 to search entry by name: " 
+                        + "\n Enter 4 to delete an entry: "
                         + "\n Enter Any other number to exit...");
 
                 String userSelection = scanner.nextLine();
@@ -70,8 +69,32 @@ public class Main {
                         System.err.println("\n Name: " + nameSearch + " Phone no: " + phoneBook.get(nameSearch)+"\n");
                     }else{
                         System.out.println("Name not found");
+                        
                     }
+                }else if(userSelection.equals("4")){
+                    System.out.println("\nEnter the name: ");
+                    String nameDelete = scanner.nextLine();
+
+                    if(phoneBook.containsKey(nameDelete)){
+                        System.out.println("Name found!");
+                        System.out.println("\n Are you sure?");
+                        String answer = scanner.nextLine();
+
+                        if(answer.equalsIgnoreCase("yes")){
+                            phoneBook.remove(nameDelete);
+                            System.out.println("Entry delete successfull!");
+                        }else if (answer.equalsIgnoreCase("no")){
+                            break;
+                        }else {
+                            System.out.println("Invalid Input!");
+                        }
+                    }else {
+                        System.out.println("\nName not found!");
+                    }
+                }else{
+                    break;
                 }
+                
 
             }
         } catch (Exception e) {
