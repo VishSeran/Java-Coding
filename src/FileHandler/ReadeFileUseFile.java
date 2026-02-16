25d7073cc8aa9bef06841c3d40bb2de86fdfbf85
VishSeran <vishwa.sovis@gmail.com>
2026-02-16 12:04:22 +0530

    update file reading files

---
+++ src/FileHandler/ReadeFileUseFile.java
@@ -0,0 +1,27 @@
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+
+public class ReadeFileUseFile {
+    public static void main (String []args){
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the file path: ");
+
+        Path path = Paths.get(scanner.nextLine());
+
+        try {
+            String content = Files.readString(path);
+
+            System.out.println("Content: " + content );
+
+        } catch (IOException e) {
+            System.out.println("Invalid input");
+        }
+
+    }    
+}
